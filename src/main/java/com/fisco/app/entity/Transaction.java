cea17a47e5cbe54fcc7d981abef06cf448f8e5f4
chengqingtan <chengqingtan@gmail.com>
2023-07-04 16:12:32 +0800

    update Transaction.java, remove it's transaction_id attribute.

--- src/main/java/com/fisco/app/entity/Transaction.java
+++ src/main/java/com/fisco/app/entity/Transaction.java
@@ -3,14 +3,10 @@ package com.fisco.app.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.nio.file.Path;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 public class Transaction {
-    private Integer transaction_id;
     private Integer pet_id;
     private String purchase_username;
     private String sell_username;
