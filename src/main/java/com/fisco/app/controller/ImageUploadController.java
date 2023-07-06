cb8f278341c2a468bca3942b4e7332d806e08533
chengqingtan <chengqingtan@gmail.com>
2023-07-06 18:43:27 +0800

    修改图片上传接口，允许接受.jpg和.png后缀的图片

--- src/main/java/com/fisco/app/controller/ImageUploadController.java
+++ src/main/java/com/fisco/app/controller/ImageUploadController.java
@@ -23,38 +23,43 @@ public class ImageUploadController {
     private String imageSavePath;
 
     @PostMapping("/upload_image")
-    public ResponseData fileupload(MultipartFile file, HttpServletRequest req){
+    public ResponseData fileUpload(MultipartFile file, HttpServletRequest req){
 
-        //获取文件的名字
+        //获取文件全名(含拓展名)
         String originName = file.getOriginalFilename();
-        System.out.println("originName:"+originName);
-        //判断文件类型
-        if(!originName.endsWith(".jpg")) {
-            return ResponseData.error("文件类型错误，请上传png或jpg");
+        //获取文件名不含后缀
+        String fileName = originName.substring(0,file.getOriginalFilename().lastIndexOf("."));
+        //获取文件后缀名(如.jpg .png)
+        String fileSuffix = originName.substring(file.getOriginalFilename().lastIndexOf("."));
+        if (".jpg".equals(fileSuffix) || ".png".equals(fileSuffix)) {
+            //如果以 .jpg 或 .png 结尾
+            //给上传的文件新建目录，目录名命名为当前日期，格式为 yyyy.MM.dd
+            String format = sdf.format(new Date());
+            String realPath = imageSavePath + format;
+            System.out.println("realPath:"+realPath);
+            //若目录不存在则创建目录
+            File folder = new File(realPath);
+            if(!folder.exists()) {
+                folder.mkdirs();
+            }
+            //给上传文件取新的名字，避免重名，随机产生UUID作为文件名
+            String newName = UUID.randomUUID().toString() + fileSuffix;
+            try {
+                //生成文件，folder为文件目录，newName为文件名
+                file.transferTo(new File(folder,newName));
+                //生成返回给前端的url
+                String url = req.getScheme() + "://" + req.getServerName() + ":" + req.getServerPort() +"/images"+ format + newName;
+                System.out.println("url:"+url);
+                //返回URL
+                return ResponseData.success(url);
+            }catch (IOException e) {
+                // TODO Auto-generated catch block
+                return ResponseData.error("图片上传失败");
+            }
         }
-        //给上传的文件新建目录
-        String format = sdf.format(new Date());
-        String realPath = imageSavePath + format;
-        System.out.println("realPath:"+realPath);
-        //若目录不存在则创建目录
-        File folder = new File(realPath);
-        if(! folder.exists()) {
-            folder.mkdirs();
+        else {
+            //如果不是以 .jpg 或 .png 结尾，不是图片
+            return ResponseData.error("文件类型错误，请上传png或jpg!");
         }
-        //给上传文件取新的名字，避免重名
-        String newName = UUID.randomUUID().toString() + ".jpg";
-        try {
-            //生成文件，folder为文件目录，newName为文件名
-            file.transferTo(new File(folder,newName));
-            //生成返回给前端的url
-            String url = req.getScheme() + "://" + req.getServerName() + ":" + req.getServerPort() +"/images"+ format + newName;
-            System.out.println("url:"+url);
-            //返回URL
-            return ResponseData.success(url);
-        }catch (IOException e) {
-            // TODO Auto-generated catch block
-            return ResponseData.error("图片上传失败");
-        }
-
     }
 }
