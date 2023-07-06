e00bb74ace87d1f401bc1a73300a0a27ed18117d
chengqingtan <chengqingtan@gmail.com>
2023-07-06 18:12:05 +0800

    为数据库添加tb_pet_class即宠物种类表

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -1,3 +1,4 @@
+drop table if exists tb_pet_class;
 drop table if exists tb_pet;
 drop table if exists tb_user_profile;
 drop table if exists tb_user;
@@ -15,13 +16,19 @@ create table if not exists tb_user
 
 create table if not exists tb_user_profile
 (
-    username varchar(10) primary key,
-    birthday date,
-    gender varchar(10) default 'hide' CHECK ( gender in ('male', 'female', 'hide') ),
-    avatar_url varchar(50),
+    username    varchar(10) primary key,
+    birthday    date,
+    gender      varchar(10) default 'hide' CHECK ( gender in ('male', 'female', 'hide') ),
+    avatar_url  varchar(50),
     description text,
     CONSTRAINT username_constraint
-    FOREIGN KEY (username) REFERENCES tb_user (username)
+        FOREIGN KEY (username) REFERENCES tb_user (username)
+);
+
+create table if not exists tb_pet_class
+(
+    pet_class   varchar(10) primary key,
+    description text
 );
 
 create table if not exists tb_pet
@@ -29,11 +36,13 @@ create table if not exists tb_pet
     pet_id       int primary key auto_increment,
     pet_name     varchar(10),
     owner        varchar(10),
-    image_url varchar(50),
+    image_url    varchar(50),
     description  varchar(20),
     price        int,
     pet_class    varchar(10),
     has_sold_out boolean default false,
     CONSTRAINT owner_constraint
-        FOREIGN KEY (owner) REFERENCES tb_user (username)
+        FOREIGN KEY (owner) REFERENCES tb_user (username),
+    CONSTRAINT pet_class_constraint
+        FOREIGN KEY (pet_class) REFERENCES tb_pet_class (pet_class)
 );
