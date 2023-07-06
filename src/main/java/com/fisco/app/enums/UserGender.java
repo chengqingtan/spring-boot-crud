0b5f0fca4e1ad8bad9c3dcc102f36c9de7775344
chengqingtan <chengqingtan@gmail.com>
2023-07-06 18:05:50 +0800

    添加用户性别枚举类

---
+++ src/main/java/com/fisco/app/enums/UserGender.java
@@ -0,0 +1,12 @@
+package com.fisco.app.enums;
+
+/**
+ * 用户性别
+ */
+public class UserGender {
+    public static final String GENDER_MALE = "male";
+    public static final String GENDER_FEMALE = "female";
+    public static final String GENDER_HIDE = "hide";
+
+    private UserGender() {}
+}
